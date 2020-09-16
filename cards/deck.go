3b1665a5a54e77d3d9a292cfbf864f537d4d676f
renso3x <enso123456@gmail.com>
2020-09-16 16:01:58 +0800

    add feature shuffle deal, savetoFile, readfrom file, generaterandom number

--- cards/deck.go
+++ cards/deck.go
@@ -1,11 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"os"
+	"strings"
+	"time"
+)
 
 // Create a new type of 'deck'
 // a slice of strings
 type deck []string
 
+// Create and return a list of deck
 func newDeck() deck {
 	cards := deck{}
 
@@ -21,8 +29,52 @@ func newDeck() deck {
 }
 
 // function receiver just 1-3letter for initializing
+// works like this in JS
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
+	}
+}
+
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+// Return a deck list of string
+func newDeckFromFile(filename string) deck {
+	//byteslice and error
+	bs, err := ioutil.ReadFile(filename)
+
+	if err != nil {
+		fmt.Println("Error: ", err)
+		// exit the program
+		os.Exit(1)
+	}
+
+	// convert the byte slice to a slice of string
+	s := strings.Split(string(bs), ",")
+
+	return deck(s)
+}
+
+func (d deck) shuffle() {
+
+	generateRandomNumber := time.Now().UnixNano()
+
+	source := rand.NewSource(generateRandomNumber)
+	r := rand.New(source)
+
+	for i := range d {
+		newPosition := r.Intn(len(d) - 1)
+
+		d[i], d[newPosition] = d[newPosition], d[i]
 	}
 }
--- cards/main.go
+++ cards/main.go
@@ -2,6 +2,12 @@ package main
 
 func main() {
 	cards := newDeck()
-
+	cards.shuffle()
+	// cards.saveToFile("my_cards")
 	cards.print()
+	// hand, remainingCards := deal(cards, 5)
+	// hand.print()
+	// fmt.Println("remainingCards")
+	// remainingCards.print()
+
 }
